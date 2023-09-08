decbd5b81839435df828d32ed07e3c0fd3e17f28
xiaodan01 <65934452+xiaodan01@users.noreply.github.com>
2023-09-09 07:51:07 +0800

    Add files via upload

--- bot/beandata.py
+++ bot/beandata.py
@@ -40,20 +40,14 @@ def gen_params(page):
     }
     return params
 
-
 def get_beans_7days(ck):
     try:
         day_7 = True
         page = 0
-        headers = {
-            "Host": "api.m.jd.com",
-            "Connection": "keep-alive",
-            "charset": "utf-8",
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2797 MMWEBSDK/201201 Mobile Safari/537.36 MMWEBID/7986 MicroMessenger/8.0.1840(0x2800003B) Process/appbrand4 WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64 MiniProgramEnv/android",
-            "Content-Type": "application/x-www-form-urlencoded;",
-            "Accept-Encoding": "gzip, compress, deflate, br",
-            "Cookie": ck,
-            "Referer": "https://servicewechat.com/wxa5bf5ee667d91626/141/page-frame.html",
+        headers = {            
+            "Content-Type": "application/x-www-form-urlencoded;",           
+            "User-Agent": "Mozilla/5.0 (Linux; Android 12; SM-G9880) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Mobile Safari/537.36 EdgA/106.0.1370.47",
+            "Cookie": ck
         }
         days = []
         for i in range(0, 7):
@@ -61,23 +55,26 @@ def get_beans_7days(ck):
                 (datetime.date.today() - datetime.timedelta(days=i)).strftime("%Y-%m-%d"))
         beans_in = {key: 0 for key in days}
         beans_out = {key: 0 for key in days}
+        
         while day_7:
-            page = page + 1
-            resp = session.get(url, params=gen_params(page),
-                               headers=headers, timeout=100).text
+            page = page + 1            
+            url="https://bean.m.jd.com/beanDetail/detail.json?page="+str(page)
+            resp = session.get(url,headers=headers, timeout=100).text           
+            amount=0
             res = json.loads(resp)
-            if res['resultCode'] == 0:
-                for i in res['data']['list']:
-                    for date in days:
-                        if str(date) in i['createDate'] and i['amount'] > 0:
+            if res['code'] == "0":
+                for i in res['jingDetailList']:
+                    amount=int(i['amount'])
+                    for date in days:                        
+                        if str(date) in i['date'] and amount > 0:
                             beans_in[str(date)] = beans_in[str(
-                                date)] + i['amount']
+                                date)] + amount
                             break
-                        elif str(date) in i['createDate'] and i['amount'] < 0:
+                        elif str(date) in i['date'] and amount < 0:
                             beans_out[str(date)] = beans_out[str(
-                                date)] + i['amount']
+                                date)] + amount
                             break
-                    if i['createDate'].split(' ')[0] not in str(days):
+                    if i['date'].split(' ')[0] not in str(days):                                                
                         day_7 = False
             else:
                 return {'code': 400, 'data': res}
@@ -86,22 +83,22 @@ def get_beans_7days(ck):
         logger.error(str(e))
         return {'code': 400, 'data': str(e)}
 
-
 def get_total_beans(ck):
     try:
         headers = {
-            "Host": "wxapp.m.jd.com",
+            "Accept": "application/json,text/plain, */*",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-cn",
             "Connection": "keep-alive",
-            "charset": "utf-8",
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2797 MMWEBSDK/201201 Mobile Safari/537.36 MMWEBID/7986 MicroMessenger/8.0.1840(0x2800003B) Process/appbrand4 WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64 MiniProgramEnv/android",
-            "Content-Type": "application/x-www-form-urlencoded;",
-            "Accept-Encoding": "gzip, compress, deflate, br",
             "Cookie": ck,
+            "Referer": "https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 12; SM-G9880) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Mobile Safari/537.36 EdgA/106.0.1370.47"
         }
-        jurl = "https://wxapp.m.jd.com/kwxhome/myJd/home.json"
+        jurl = "https://wq.jd.com/user/info/QueryJDUserInfo?sceneval=2"
         resp = session.get(jurl, headers=headers, timeout=100).text
-        res = json.loads(resp)
-        return res['user']['jingBean']
+        res = json.loads(resp)       
+        return res['base']['jdNum'],res['base']['nickname'],'http://storage.360buyimg.com/i.imageUpload/b6adc6e4bbaa31363437393935323238323435_mid.jpg'
     except Exception as e:
         logger.error(str(e))
 
@@ -115,7 +112,7 @@ def get_bean_data(i):
         if cookies:
             ck = cookies[i-1]
             beans_res = get_beans_7days(ck)
-            beantotal = get_total_beans(ck)
+            beantotal,nickname,pic = get_total_beans(ck)
             if beans_res['code'] != 200:
                 return beans_res
             else:
@@ -127,6 +124,6 @@ def get_bean_data(i):
                     beans_in.append(int(beans_res['data'][0][i]))
                     beans_out.append(int(str(beans_res['data'][1][i]).replace('-', '')))
                     beanstotal.append(beantotal)
-            return {'code': 200, 'data': [beans_in[::-1], beans_out[::-1], beanstotal[::-1], beans_res['data'][2][::-1]]}
+            return {'code': 200, 'data': [beans_in[::-1], beans_out[::-1], beanstotal[::-1], beans_res['data'][2][::-1],nickname,pic]}
     except Exception as e:
         logger.error(str(e))
--- bot/getfile.py
+++ bot/getfile.py
@@ -1,106 +1,131 @@
 from telethon import events, Button
-import asyncio
+from asyncio import exceptions
 from .. import jdbot, chat_id, SCRIPTS_DIR, CONFIG_DIR, logger
-from .utils import press_event, backup_file, add_cron, cmd, DIY_DIR, TASK_CMD, V4, split_list, row
-
+from .utils import press_event, backup_file, Remove_file, add_cron, cmd, DIY_DIR, TASK_CMD, split_list
+import json
+import os
 
 @jdbot.on(events.NewMessage(from_users=chat_id))
 async def bot_get_file(event):
-    """
-    定义文件操作
-    """
-    try:
-        if not event.message.file:
-            return
-        filename = event.message.file.name
+    """定义文件操作"""
+    try:        
+        btn = []
+        issetconfig=False
+        
+        if os.path.exists("/ql/data/config/auth.json"):
+            configpath="/ql/data/"
+            
+        if os.path.exists("/ql/config/auth.json"):
+            configpath="/ql/"
+            
+        if os.path.exists("/jd/config/config.sh"):
+            configpath="/jd/"
+            
         try:
-            if not (
-                filename.endswith(".py")
-                or filename.endswith(".pyc")
-                or filename.endswith(".js")
-                or filename.endswith(".sh")
-            ):
-                return
-        except:
+            f = open(configpath+"config/ccbotSetting.json", "r+", encoding='utf-8')
+            ccbotSetting = json.loads(f.read())
+            f.close()
+            for key in ccbotSetting:
+                if key=="文件存放配置":
+                    issetconfig=True
+        except Exception as e:
+            await jdbot.send_message(chat_id,f'载入ccbotSetting.json出错,请检查内容!\n'+str(e))
             return
-        SENDER = event.sender_id
-        cmdtext = False
-        if V4:
-            buttons = [
-                Button.inline('放入config', data=CONFIG_DIR),
-                Button.inline('仅放入scripts', data=SCRIPTS_DIR),
-                Button.inline('仅放入own文件夹', data=DIY_DIR),
-                Button.inline('放入scripts并运行', data='node1'),
-                Button.inline('放入own并运行', data='node'),
-                Button.inline('取消', data='cancel')
-            ]
-        else:
-            buttons = [
-                Button.inline('放入config', data=CONFIG_DIR),
-                Button.inline('仅放入scripts', data=SCRIPTS_DIR),
-                Button.inline('放入scripts并运行', data='node1'),
-                Button.inline('取消', data='cancel')
-            ]
-        async with jdbot.conversation(SENDER, timeout=180) as conversation:
-            msg = await conversation.send_message("请选择您要放入的文件夹或操作：\n", buttons=split_list(buttons, row))
-            byte = await conversation.wait_event(press_event(SENDER))
-            res1 = bytes.decode(byte.data)
-            if res1 == "cancel":
-                await jdbot.edit_message(msg, "对话已取消")
-                conversation.cancel()
-                return
-            await jdbot.delete_messages(chat_id, msg)
-            buttons = [Button.inline('是', data='yes'), Button.inline('否', data='no')]
-            msg = await conversation.send_message("是否尝试自动加入定时", buttons=buttons)
-            byte = await conversation.wait_event(press_event(SENDER))
-            res2 = bytes.decode(byte.data)
-            if res2 == "cancel":
-                await jdbot.edit_message(msg, "对话已取消")
-                conversation.cancel()
-                return
-            if res1 == "node":
-                backup_file(f'{DIY_DIR}/{filename}')
-                await jdbot.download_media(event.message, DIY_DIR)
-                cmdtext = f'{TASK_CMD} {DIY_DIR}/{filename} now'
-                if res2 == 'yes':
-                    try:
-                        with open(f'{DIY_DIR}/{filename}', 'r', encoding='utf-8') as f:
-                            resp = f.read()
-                    except:
-                        resp = "None"
-                    await add_cron(jdbot, conversation, resp, filename, msg, SENDER, buttons, DIY_DIR)
-                else:
-                    await jdbot.edit_message(msg, '脚本已保存到DIY文件夹，并成功运行')
-            elif res1 == "node1":
-                backup_file(f'{SCRIPTS_DIR}/{filename}')
-                await jdbot.download_media(event.message, SCRIPTS_DIR)
-                cmdtext = f'{TASK_CMD} {SCRIPTS_DIR}/{filename} now'
-                if res2 == 'yes':
-                    try:
-                        with open(f'{SCRIPTS_DIR}/{filename}', 'r', encoding='utf-8') as f:
-                            resp = f.read()
-                    except:
-                        resp = "None"
-                    await add_cron(jdbot, conversation, resp, filename, msg, SENDER, buttons, SCRIPTS_DIR)
-                else:
-                    await jdbot.edit_message(msg, '脚本已保存到SCRIPTS文件夹，并成功运行')
+            
+        if not issetconfig:
+            await jdbot.send_message(chat_id,f'载入ccbotSetting.json成功，但是缺少相应的配置,请检查!')
+            return
+        
+        getfileSettinglist=ccbotSetting["文件存放配置"]
+        
+        countbtn=3
+        for fileSetting in getfileSettinglist: 
+            if fileSetting["按钮名字"]=="配置档":
+                countbtn=int(fileSetting["每行按钮数"])
             else:
-                backup_file(f'{res1}/{filename}')
-                await jdbot.download_media(event.message, res1)
-                if res2 == 'yes':
-                    try:
-                        with open(f'{res1}/{filename}', 'r', encoding='utf-8') as f:
-                            resp = f.read()
-                    except:
-                        resp = "None"
-                    await add_cron(jdbot, conversation, resp, filename, msg, SENDER, buttons, res1)
+                btn.append(Button.inline(fileSetting["按钮名字"], data=fileSetting["按钮名字"]+"|"+fileSetting["存放路径"]))          
+        btn.append(Button.inline('取消', data='取消|cancel'))
+        btn = split_list(btn, countbtn)   
+        
+        SENDER = event.sender_id
+        if event.message.file:
+            markup = []
+            filename = event.message.file.name
+            cmdtext = None
+            runcmd = ""
+            async with jdbot.conversation(SENDER, timeout=180) as conv:
+                msg = await conv.send_message('请选择您要放入的文件夹或操作：\n')
+                markup = btn
+                
+                msg = await jdbot.edit_message(msg, '请选择您要放入的文件夹或操作：', buttons=markup)
+                convdata = await conv.wait_event(press_event(SENDER))
+                
+                res = bytes.decode(convdata.data)
+                isbackup="1"
+                noaskaddcron="0"
+                
+                for fileSetting in getfileSettinglist: 
+                    if fileSetting["按钮名字"]=="配置档":
+                        continue
+                    if fileSetting["按钮名字"]==res.split("|")[0]:
+                        isbackup=fileSetting["备份原脚本"]                        
+                        for key in fileSetting:
+                            if "执行命令" in key:
+                                if runcmd!="" :     
+                                    runcmd=runcmd+"\n"
+                                runcmd=runcmd+fileSetting[key].replace("文件名",filename)
+                            if "不问是否定时" in key:  
+                                noaskaddcron=fileSetting[key]
+                isrun="0"
+                res=res.split("|")[1]
+                if "task " in res:
+                    isrun="1"
+                    res=res.replace("task ","")
+                    
+                markup = [Button.inline('是', data='yes'),
+                          Button.inline('否', data='no')]
+                if res == 'cancel':
+                    msg = await jdbot.edit_message(msg, '对话已取消')
+                    conv.cancel()
                 else:
-                    await jdbot.edit_message(msg, f'{filename}已保存到{res1}文件夹')
-            conversation.cancel()
-        if cmdtext:
-            await cmd(cmdtext)
-    except asyncio.TimeoutError:
-        await jdbot.edit_message(msg, '选择已超时，对话已停止')
+                    res2=""
+                    if noaskaddcron=="0":
+                        msg = await jdbot.edit_message(msg, '是否尝试自动加入定时', buttons=markup)
+                        convdata2 = await conv.wait_event(press_event(SENDER))
+                        res2 = bytes.decode(convdata2.data)
+                    
+                    if isbackup=="1":
+                        backup_file(f'{res}/{filename}')
+                    else:
+                        Remove_file(f'{res}/{filename}')
+                        
+                    if isrun=="1":    
+                        cmdtext = f'{TASK_CMD} {res}/{filename} now'
+                    await jdbot.download_media(event.message, res)
+                    with open(f'{res}/{filename}', 'r', encoding='utf-8') as f:
+                        resp = f.read()
+                        
+                    if res2 == 'yes':
+                        await add_cron(jdbot, conv, resp, filename, msg, SENDER, markup, res)
+                    else:
+                        await jdbot.edit_message(msg, f'{filename}已保存到{res}文件夹')
+                    conv.cancel()
+            if cmdtext:
+                if runcmd!="":     
+                    runcmd=cmdtext+"\n"+runcmd
+                else:
+                    runcmd=cmdtext
+                
+            if runcmd!="":                 
+                msg=await jdbot.send_message(chat_id,"开始执行命令列表"+":\n"+runcmd)
+                cmdlist=runcmd.split("\n")
+                for RunCommound in cmdlist: 
+                    await cmd(RunCommound)
+                    
+                await jdbot.edit_message(msg, '任务执行完毕，祝君愉快.')    
+                    
+    except exceptions.TimeoutError:
+        msg = await jdbot.send_message(chat_id, '选择已超时，对话已停止')
     except Exception as e:
         await jdbot.send_message(chat_id, f'something wrong,I\'m sorry\n{str(e)}')
         logger.error(f'something wrong,I\'m sorry\n{str(e)}')
