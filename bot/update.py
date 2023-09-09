87d55fac50a8284d8eae3b836a150233f06bf733
xiaodan01 <65934452+xiaodan01@users.noreply.github.com>
2023-09-09 08:18:53 +0800

    Update update.py

--- bot/update.py
+++ bot/update.py
@@ -1,6 +1,6 @@
-version = 'v1.4.0'
+version = 'v2.0'
 botlog = """
-2022.8.9 (v1.4.0)
-    * 修复报错问题(Empty or invalid UTF-8 xxxx)
-[更多](https://github.com/curtinlv/gd/blob/main/updateLog.md)
+2023.9.9 (v2.0)
+    * 修复更新
+[更多](https://github.com/xiaodan01/gd/blob/main/updateLog.md)
 """
